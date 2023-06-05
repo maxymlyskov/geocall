22159ddd85a49c9646b321dda65a137ab0f3ed3b
maxymlyskov <raypember02@gmail.com>
2023-06-05 17:21:27 +0300

    Got geo location.

--- front/src/LoginPage/LoginPage.jsx
+++ front/src/LoginPage/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoginButton from "./LoginButton";
 import LoginInput from "./LoginInput";
@@ -7,13 +7,34 @@ import Logo from "./Logo";
 const isUserNameValid = (userName) => {
   return userName.length > 0 && userName.length < 10 && !userName.includes(" ");
 };
+const locationOptions = {
+  enableHighAcciracy: true,
+  timeout: 5000,
+  maximumAge: 0,
+};
 
 const LoginPage = () => {
   const [userName, setUserName] = useState("");
   const navigate = useNavigate();
+  const onSuccess = (position) => {
+    console.log(position);
+  };
+
+  const onError = (error) => {
+    console.log("error: " + error);
+  };
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      onSuccess,
+      onError,
+      locationOptions
+    );
+  }, []);
+
   const handleLogin = () => {
     navigate("/map");
   };
+
   return (
     <div className="l_page_main_container">
       <div className="l_page_box">
