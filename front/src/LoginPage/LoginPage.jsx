d4a85ed9580d0201242574aa213b10b4c9cbf907
maxymlyskov <raypember02@gmail.com>
2023-06-05 17:28:29 +0300

    Added geo coords dispatch.

--- front/src/LoginPage/LoginPage.jsx
+++ front/src/LoginPage/LoginPage.jsx
@@ -3,6 +3,8 @@ import { useNavigate } from "react-router-dom";
 import LoginButton from "./LoginButton";
 import LoginInput from "./LoginInput";
 import Logo from "./Logo";
+import { useDispatch } from "react-redux";
+import { setMyLocation } from "../MapPage/mapSlice";
 
 const isUserNameValid = (userName) => {
   return userName.length > 0 && userName.length < 10 && !userName.includes(" ");
@@ -17,8 +19,16 @@ const LoginPage = () => {
   const [userName, setUserName] = useState("");
   const [locationErrorOccured, setLocationErrorOccured] = useState(false);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
+
   const onSuccess = (position) => {
     console.log(position);
+    dispatch(
+      setMyLocation({
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      })
+    );
   };
 
   const onError = (error) => {
