ce875f5b73204a0141f5255def8cb5ec243cc0ca
maxymlyskov <raypember02@gmail.com>
2023-06-05 16:28:23 +0300

    Added sockets.

--- server/index.js
+++ server/index.js
@@ -3,17 +3,37 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const server = http.createServer(app);
 
 app.use(cors());
 
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
 app.get("/", (req, res) => {
   res.send("Hello");
 });
 
+io.on("connection", (socket) => {
+  console.log("user connected with id: " + socket.id);
+
+  socket.on("disconnect", () => {
+    disconnectEventHandler(socket.id);
+  });
+});
+
 const PORT = process.env.PORT || 3003;
 
-server.listen(PORT, ()=>{
-    console.log('Server is listening on port ' + PORT)
+server.listen(PORT, () => {
+  console.log("Server is listening on port " + PORT);
 });
+
+const disconnectEventHandler = (id) => {
+  console.log("user disconnected with id: " + id);
+};
