908c40c3df04c4e19c20f6b6d79d3450fba8807d
maxymlyskov <raypember02@gmail.com>
2023-06-09 15:08:22 +0300

    Added local video display.

--- front/src/VideoRooms/VideoRooms.jsx
+++ front/src/VideoRooms/VideoRooms.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import RoomsList from "./RoomsList";
+import ParticipantsVideos from "./ParticipantsVideos";
 
 const VideoRooms = () => {
-  return <RoomsList />;
+  return (
+    <>
+      <RoomsList />
+      <ParticipantsVideos />
+    </>
+  );
 };
 
 export default VideoRooms;
